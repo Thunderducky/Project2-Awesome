9978d1348c24fe270a88409f0f80894d0bc05598
Eric Scott <Thunderducky@users.noreply.github.com>
2020-04-27 11:23:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require("./routes/api-routes.js")(app);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("A small change");
+    console.log('A small change');
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
